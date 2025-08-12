3dc144e1233a85fc410041de9fd8f319dab52bc0
pothagoniharish <146047342+pothagoniharish@users.noreply.github.com>
2025-08-12 11:00:23 -0500

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-123456"
+    bucket = "dev-proj-1-remote-state-bucket-001"
     key    = "devops-project-1/terraform.tfstate"
     region = "us-east-2"
   }
